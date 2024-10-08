3a271699106e812b5621513c41bb4399de6b7080
Netvoiangel <fatahov.t123@gmail.com>
2024-10-08 13:22:26 +0300

    Ternary operator example completed

---
+++ 7.ConditionalTransitionOperator/TernaryOperatorExample.java
@@ -0,0 +1,7 @@
+public class TernaryOperatorExample {
+    public static void main(String[] args) {
+        int a = 0; 
+        int i = (a == 1) ? 1 : (a == 2) ? 2 : 3;
+        System.out.println(i);
+    }
+}
